64b6b7ed1c25281d2c20833a14d6216249c98615
zhozhyr <140246410+zhozhyr@users.noreply.github.com>
2024-03-17 20:45:34 +0700

    lestgo pattern

--- pythonplay/pythonplay_app/urls.py
+++ pythonplay/pythonplay_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, LetsgoPageView
 
 urlpatterns = (
     path('', HomePageView.as_view(), name='home'),
-
+    path('/letsgo', LetsgoPageView.as_view(), name='letsgo'),
 )
