f94d6daeb5847ba1aa0ecd0c43aa51b12249c88a
zhozhyr <140246410+zhozhyr@users.noreply.github.com>
2024-03-31 20:18:35 +0700

    фикс urls+views, доработка уровня и roadmap

--- pythonplay/pythonplay_app/urls.py
+++ pythonplay/pythonplay_app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import HomePageView, LetsgoPageView, LoginPageView, RoadmapPageView, ProfilePageView
+from .views import HomePageView, LetsgoPageView, LoginPageView, RoadmapPageView, Level_0_View, ProfilePageView
 
 urlpatterns = (
     path('', HomePageView.as_view(), name='home'),
     path('letsgo', LetsgoPageView.as_view(), name='letsgo'),
     path('login', LoginPageView.as_view(), name='login'),
     path('roadmap', RoadmapPageView.as_view(), name='roadmap'),
+    path('level0/', Level_0_View.as_view(), name='level0'),
     path('profile', ProfilePageView.as_view(), name='profile'),
 )
--- pythonplay/pythonplay_app/views.py
+++ pythonplay/pythonplay_app/views.py
@@ -20,3 +20,18 @@ class RoadmapPageView(TemplateView):
 class ProfilePageView(TemplateView):
     template_name = 'profile.html'
 
+def create_level_view(level_number):
+    class_name = f"Level_{level_number}_View"
+    template_name = f"level{level_number}.html"
+
+    # Создание класса динамически с помощью type()
+    dynamic_class = type(
+        class_name,
+        (TemplateView,),
+        {'template_name': template_name}
+    )
+
+    return dynamic_class
+
+Level_0_View = create_level_view(0)
+Level_1_View = create_level_view(1)
