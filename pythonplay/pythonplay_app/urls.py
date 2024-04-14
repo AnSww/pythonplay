f004d716b5c7b70c6197c0380f02e917647afb04
zhozhyr <140246410+zhozhyr@users.noreply.github.com>
2024-04-14 13:53:16 +0700

    шаблон уровня №1 + шаблон уведомления

--- pythonplay/pythonplay_app/urls.py
+++ pythonplay/pythonplay_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import HomePageView, LetsgoPageView, LoginPageView, RoadmapPageView, Level_0_View, ProfilePageView
+from .views import HomePageView, LetsgoPageView, LoginPageView, RoadmapPageView, Level_0_View, ProfilePageView, \
+    Level_1_View
 
 urlpatterns = (
     path('', HomePageView.as_view(), name='home'),
@@ -7,5 +8,6 @@ urlpatterns = (
     path('login', LoginPageView.as_view(), name='login'),
     path('roadmap', RoadmapPageView.as_view(), name='roadmap'),
     path('level0/', Level_0_View.as_view(), name='level0'),
+    path('level1/', Level_1_View.as_view(), name='level1'),
     path('profile', ProfilePageView.as_view(), name='profile'),
 )
