a7a22598a6d9ea9339f5937f842edc48555d9e73
Aleksandra255685 <sashaven2003@mail.ru>
2024-04-25 22:01:47 +0700

    окно с инфо о получении сертификата + доработка формы настроек

--- pythonplay/static/js/settings_popup.js
+++ pythonplay/static/js/settings_popup.js
@@ -1,18 +1,41 @@
+//Settings
 // Открыть модальное окно
-document.getElementById("open-modal-btn").addEventListener("click", function() {
-    document.getElementById("my-modal").classList.add("open")
+document.getElementById("open-settings-btn").addEventListener("click", function() {
+    document.getElementById("settings-modal").classList.add("open")
 })
 
 // Закрыть модальное окно
-document.getElementById("close-my-modal-btn").addEventListener("click", function() {
-    document.getElementById("my-modal").classList.remove("open")
+document.getElementById("close-settings-btn").addEventListener("click", function() {
+    document.getElementById("settings-modal").classList.remove("open")
 })
 
 // Закрыть модальное окно при клике вне его
-document.querySelector("#my-modal .modal__box").addEventListener('click', event => {
+document.querySelector("#settings-modal .modal__box").addEventListener('click', event => {
     event._isClickWithInModal = true;
 });
-document.getElementById("my-modal").addEventListener('click', event => {
+document.getElementById("settings-modal").addEventListener('click', event => {
+    if (event._isClickWithInModal) return;
+    event.currentTarget.classList.remove('open');
+});
+
+
+
+//Certificate
+// Открыть модальное окно
+document.getElementById("open-certificate-btn").addEventListener("click", function() {
+    document.getElementById("certificate-modal").classList.add("open")
+})
+
+// Закрыть модальное окно
+document.getElementById("close-certificate-btn").addEventListener("click", function() {
+    document.getElementById("certificate-modal").classList.remove("open")
+})
+
+// Закрыть модальное окно при клике вне его
+document.querySelector("#certificate-modal .modal__certificate").addEventListener('click', event => {
+    event._isClickWithInModal = true;
+});
+document.getElementById("certificate-modal").addEventListener('click', event => {
     if (event._isClickWithInModal) return;
     event.currentTarget.classList.remove('open');
 });
