b87925f9a87390b7c283c953fc074b21ac87100e
Aleksandra255685 <sashaven2003@mail.ru>
2024-04-28 15:51:20 +0700

    редактор 2.0

--- pythonplay/static/js/code_ex.js
+++ pythonplay/static/js/code_ex.js
@@ -13,27 +13,82 @@ const editor = CodeMirror.fromTextArea(document.getElementById("code"), {
 
 editor.setValue(`name = input("Введите ваше имя: ")
 print("Привет,", name)`);
-output.value = " ";
+
+
+function addToOutput(s) {
+    output.value += `${s}\n`;
+}
+
+const inputPromise = () => {
+  let variable = " ";
+
+  setTimeout(function() {
+	output.oninput = function(){
+    variable = output.value;
+  }
+  }, 1000000);
+
+
+  return variable;
+};
+
+async function input(){
+    let variable = await inputPromise();
+    return variable;
+
+}
+
 
 async function main() {
-    let pyodide = await loadPyodide({ indexURL: "https://cdn.jsdelivr.net/pyodide/v0.18.1/full/" });
+    let pyodide = await loadPyodide({ indexURL: "https://cdn.jsdelivr.net/pyodide/v0.18.1/full/",
+                                      stdin: input});
     // Pyodide ready
     return pyodide;
 };
 
 let pyodideReadyPromise = main();
 
-function addToOutput(s) {
-    output.value += s + "\n";
-}
 
 async function evaluatePython() {
+    output.value = "";
     let pyodide = await pyodideReadyPromise;
+    setup_pyodide(startcode, pyodide);
     try {
         console.log(editor.getValue())
-        let output = pyodide.runPython(editor.getValue());
+        let pycode = editor.getValue();
+        pyodide.globals.code_to_run = pycode;
+
+        let output = (pyodide.runPython('run_code(code_to_run)'))
         addToOutput(output);
     } catch (err) {
         addToOutput(err);
     }
 }
+
+
+
+let startcode = `
+import sys, io, traceback
+namespace = {}  # use separate namespace to hide run_code, modules, etc.
+def run_code(code):
+    """run specified code and return stdout and stderr"""
+    out = io.StringIO()
+    oldout = sys.stdout
+    olderr = sys.stderr
+    sys.stdout = sys.stderr = out
+    try:
+        # change next line to exec(code, {}) if you want to clear vars each time
+        exec(code, {})
+    except:
+        traceback.print_exc()
+
+    sys.stdout = oldout
+    sys.stderr = olderr
+    return out.getvalue()
+`
+
+function setup_pyodide(startcode, pyodide) {
+	// setup pyodide environment to run code blocks as needed
+	pyodide.runPython(startcode)
+  }
+
