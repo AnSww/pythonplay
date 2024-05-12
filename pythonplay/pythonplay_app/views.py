c2bc6f74a96b0596bb509e57d232f1d419a6b585
zhozhyr <140246410+zhozhyr@users.noreply.github.com>
2024-05-13 02:03:35 +0700

    code editor

--- pythonplay/pythonplay_app/views.py
+++ pythonplay/pythonplay_app/views.py
@@ -35,3 +35,46 @@ def create_level_view(level_number):
 
 Level_0_View = create_level_view(0)
 Level_1_View = create_level_view(1)
+
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import subprocess
+
+
+@csrf_exempt
+def run_code(request):
+    if request.method == 'POST':
+        try:
+            code = request.POST.get('code', '')
+            language = request.POST.get('language', '')
+
+            if language == 'python':
+                input_data = request.POST.get('input', '')  # Get input data
+                result = run_python_code(code, input_data)
+            else:
+                result = "Language not supported for execution."
+
+            return JsonResponse({'result': result})
+
+        except Exception as e:
+            return JsonResponse({'error': str(e)})
+
+    return JsonResponse({'error': 'Invalid request method'})
+
+
+
+def run_python_code(code, input_data):
+    try:
+        # Use Popen to pass input to subprocess
+        process = subprocess.Popen(['python3', '-c', code], stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE, text=True)
+        stdout, stderr = process.communicate(input=input_data, timeout=5)
+
+        if stderr:
+            return stderr
+        else:
+            return stdout
+    except subprocess.TimeoutExpired:
+        return "Error: Process timed out."
+    except subprocess.CalledProcessError as e:
+        return f"Error: {e}"
