a4577195603c0fbe59b2f95b51ec83a89c07c89b
Aleksandra255685 <sashaven2003@mail.ru>
2024-05-30 13:02:19 +0700

    lvl 4-5

--- pythonplay/pythonplay_app/urls.py
+++ pythonplay/pythonplay_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import HomePageView, LetsgoPageView, LoginPageView, roadmapPageView, Level_0_View, \
-    Level_1_View, run_code, Level_2_View, Level_3_View, Level_18_View, Level_19_View, Level_20_View, check_code
+    Level_1_View, run_code, Level_2_View, Level_3_View, Level_4_View, Level_5_View, Level_18_View, Level_19_View, Level_20_View, check_code
 
 urlpatterns = (
     path('', HomePageView.as_view(), name='home'),
@@ -11,6 +11,8 @@ urlpatterns = (
     path('level1', Level_1_View.as_view(), name='level1'),
     path('level2', Level_2_View.as_view(), name='level2'),
     path('level3', Level_3_View.as_view(), name='level3'),
+    path('level4', Level_4_View.as_view(), name='level4'),
+    path('level5', Level_5_View.as_view(), name='level5'),
     path('level18', Level_18_View.as_view(), name='level18'),
     path('level19', Level_19_View.as_view(), name='level19'),
     path('level20', Level_20_View.as_view(), name='level20'),
